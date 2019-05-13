5374fa083cc812507b503fb1301dc9a20b4d2c6f
Jorge Aparicio <jorge@japaric.io>
2019-05-13 18:41:49 +0200

    fix ci

--- ci/script.sh
+++ ci/script.sh
@@ -1,24 +1,24 @@
 set -euxo pipefail
 
 main() {
-    cargo check --target $T
+    cargo check -p alloc-many --target $T
+    cargo check -p alloc-many-collections --target $T
 
-    if [ $T = x86_64-unknown-linux-gnu ]; then
-        cargo test -p alloc-many-bump
+    if [ $T = x86_64-unknown-linux-gnu ] && [ $TRAVIS_RUST_VERSION = nightly ]; then
         cargo test -p alloc-many-collections
-
-        cargo test -p alloc-many-bump --release
         cargo test -p alloc-many-collections --release
 
-        if [ $TRAVIS_RUST_VERSION = nightly ]; then
-            cd bump
-            export RUSTFLAGS="-Z sanitizer=thread"
-            export RUST_TEST_THREADS=1
-            export TSAN_OPTIONS="suppressions=$(pwd)/suppressions.txt"
+        cd bump
 
-            cargo test --test tsan --target $T
-            cargo test --test tsan --target $T --release
-        fi
+        cargo test
+        cargo test --release
+
+        export RUSTFLAGS="-Z sanitizer=thread"
+        export RUST_TEST_THREADS=1
+        export TSAN_OPTIONS="suppressions=$(pwd)/suppressions.txt"
+
+        cargo test --test tsan --target $T
+        cargo test --test tsan --target $T --release
     fi
 }
 
